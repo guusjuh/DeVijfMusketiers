27d44a1b7107f0689f9ec64d2b2307385274412c
Guus van Gend <guus.van.gend@hva.nl>
2017-11-25 23:02:53 +0100

    Added: Base action class

---
+++ Demo_WOOSH/Assets/Scripts/Level/Objects/Enemies/Actions/Action.cs
@@ -0,0 +1,41 @@
+﻿public class Action {
+    protected int totalCooldown = 0;
+    protected int currentCooldown = 0;
+
+    public virtual void Initialize()
+    {
+
+    }
+
+    public virtual void Reset()
+    {
+        currentCooldown = 0;
+    }
+
+    public virtual void Clear()
+    {
+
+    }
+
+    public virtual void StartTurn()
+    {
+        if(currentCooldown > 0){
+            currentCooldown--;
+        }
+    }
+
+    public virtual void EndTurn()
+    {
+
+    }
+
+    public virtual bool TryHit()
+    {
+        return true;
+    }
+
+    public virtual void DoAction()
+    {
+
+    }
+}
