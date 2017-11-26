d41081bdefb31ffc9de837c39b9c4e6be39f6409
Tim Reniers <tim.reniers@hotmail.com>
2017-11-26 13:04:33 +0100

    Added: ParticleManager + ParticleDestroy script

---
+++ Demo_WOOSH/Assets/Scripts/Particles/DestroyParticle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour {
+
+    ParticleSystem parts;
+
+	// Use this for initialization
+	void Start ()
+    {
+        parts = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!parts.IsAlive())
+        {
+            Destroy(transform.parent.gameObject);
+        }
+
+        if (UberManager.Instance.GameState != UberManager.GameStates.InGame)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+	}
+}
---
+++ Demo_WOOSH/Assets/Scripts/Particles/ParticleManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour {
+
+    private Dictionary<Particles, GameObject> particles = new Dictionary<Particles, GameObject>();
+
+        public enum Particles
+    {
+        FireParticle = 0,
+        IceParticle,
+        LightParticle,
+        ShrineParticle,
+        TeleportInParticle,
+        TeleportOutParticle,
+        DodinParticle,
+        ArnestParticle,
+        SkettaParticle,
+        BurnedParticle,
+        FrozenParticle,
+        ReputationParticle
+
+    }
+
+	public void Initialize()
+    {
+        particles.Add(Particles.FireParticle, Resources.Load("Prefabs/Particles/PT_FIRESPELL") as GameObject);
+        particles.Add(Particles.IceParticle, Resources.Load("Prefabs/Particles/PT_ICESPELL_v2") as GameObject);
+        particles.Add(Particles.LightParticle, Resources.Load("Prefabs/Particles/PT_LIGHTSPELL_v2") as GameObject);
+        particles.Add(Particles.ShrineParticle, Resources.Load("Prefabs/Particles/PT_SHRINE") as GameObject);
+        particles.Add(Particles.TeleportInParticle, Resources.Load("Prefabs/Particles/PT_TELEPORT_v2-IN") as GameObject);
+        particles.Add(Particles.TeleportOutParticle, Resources.Load("Prefabs/Particles/PT_TELEPORT_v2-WEG") as GameObject);
+        particles.Add(Particles.DodinParticle, Resources.Load("Prefabs/Particles/PT_DODIN") as GameObject);
+        particles.Add(Particles.ArnestParticle, Resources.Load("Prefabs/Particles/PT_ARNEST") as GameObject);
+        particles.Add(Particles.SkettaParticle, Resources.Load("Prefabs/Particles/PT_SKETTA") as GameObject);
+        particles.Add(Particles.BurnedParticle, Resources.Load("Prefabs/Particles/PT_BURNED") as GameObject);
+        particles.Add(Particles.FrozenParticle, Resources.Load("Prefabs/Particles/PT_FROZEN") as GameObject);
+        particles.Add(Particles.ReputationParticle, Resources.Load("Prefabs/Particles/PT_REPUTATION") as GameObject);
+    }
+
+    public void PlayParticle(GameManager.SpellType effect, Vector3 position, Quaternion rotation)
+    {
+        switch (effect)
+        {
+            case GameManager.SpellType.Attack:
+                PlayParticle(Particles.LightParticle, position, rotation);
+                break;
+            case GameManager.SpellType.Fireball:
+                PlayParticle(Particles.FireParticle, position, rotation);
+                break;
+            case GameManager.SpellType.FrostBite:
+                PlayParticle(Particles.IceParticle, position, rotation);
+                break;
+            case GameManager.SpellType.Teleport:
+                PlayParticle(Particles.TeleportInParticle, position, rotation);
+                break;
+        }
+    }
+
+
+    public void PlayParticle(Particles effect, Vector3 position, Quaternion rotation)
+    {
+        Instantiate(particles[effect], position, rotation);
+    }
+
+    public GameObject PlayParticleWithReturn(Particles effect, Vector3 position, Quaternion rotation)
+    {
+        return Instantiate(particles[effect], position, rotation);
+    }
+}
