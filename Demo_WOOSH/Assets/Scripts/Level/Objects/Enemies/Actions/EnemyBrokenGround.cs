c55bb09c7dbf721dd7375c377d186159ed79026b
Guus van Gend <guusvangend@hotmail.com>
2017-12-09 18:39:49 +0100

    Added: GameObjects for the visual of the new actions

--- Demo_WOOSH/Assets/Scripts/Level/Objects/Enemies/Actions/EnemyBrokenGround.cs
+++ Demo_WOOSH/Assets/Scripts/Level/Objects/Enemies/Actions/EnemyBrokenGround.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBrokenGround : Action
 {
+    GameObject[] dusts;
 
     public override void Initialize(Enemy parent)
     {
         base.Initialize(parent);
+
+        dusts = new GameObject[6];
+        
+        for (int i = 0; i < dusts.Length; i++)
+        {
+            dusts[i] = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Actions/Dust"), parent.transform);
+            dusts[i].SetActive(false);
+        }
 
         cost = 1;
     }
@@ -28,10 +38,35 @@ public class EnemyBrokenGround : Action
             {
                 node.Content.KillTileContent();
             }
+            UberManager.Instance.StartCoroutine(visual());
+
             parent.EndMove(cost);
 
             return true;
         }
         return false;
+    }
+
+    private IEnumerator visual()
+    {
+        Vector2 pos = new Vector2(parent.transform.position.x, parent.transform.position.y);
+        Coordinate coord = GameManager.Instance.TileManager.GetGridPosition(pos);
+        TileNode node = GameManager.Instance.TileManager.GetNodeReference(coord);
+        for (int i = 0; i < node.NeightBours.Count; i++)
+        {
+            dusts[i].SetActive(true);
+            Vector2 worldPos = GameManager.Instance.TileManager.GetWorldPosition(new Vector2(node.NeightBours[i].GridPosition.x, node.NeightBours[i].GridPosition.y));
+            dusts[i].transform.position = new Vector3(worldPos.x, worldPos.y, 0);
+
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < dusts.Length; i++)
+        {
+            dusts[i].SetActive(false);
+        }
+
+        yield break;
     }
 }
--- Demo_WOOSH/Assets/Scripts/Level/Objects/Enemies/Actions/EnemySelfDestruct.cs
+++ Demo_WOOSH/Assets/Scripts/Level/Objects/Enemies/Actions/EnemySelfDestruct.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySelfDestruct : Action
 {
+    GameObject[] dusts;
+
     public override void StartTurn()
     {
         base.StartTurn();
@@ -18,8 +21,35 @@ public class EnemySelfDestruct : Action
             {
                 node.Content.KillTileContent();
             }
+            UberManager.Instance.StartCoroutine(visual());
             parent.EndMove(parent.CurrentActionPoints);
-            parent.Kill();
         }
+    }
+
+    private IEnumerator visual()
+    {
+        dusts = new GameObject[7];
+
+        for (int i = 0; i < dusts.Length; i++)
+        {
+            dusts[i] = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Actions/Dust"), parent.transform);
+            dusts[i].transform.localPosition = new Vector3(0, 0, 0);
+            dusts[i].SetActive(true);
+        }
+
+        Vector2 pos = new Vector2(parent.transform.position.x, parent.transform.position.y);
+        Coordinate coord = GameManager.Instance.TileManager.GetGridPosition(pos);
+        TileNode node = GameManager.Instance.TileManager.GetNodeReference(coord);
+        for (int i = 0; i < node.NeightBours.Count; i++)
+        {
+            Vector2 worldPos = GameManager.Instance.TileManager.GetWorldPosition(new Vector2(node.NeightBours[i].GridPosition.x, node.NeightBours[i].GridPosition.y));
+            dusts[i].transform.position = new Vector3(worldPos.x, worldPos.y, 0);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        parent.Kill();
+
+        yield break;
     }
 }
